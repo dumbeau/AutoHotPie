220f361d7adbe399608fd6a09a9b270d2915835f
UnBOlievable <caspergilles@gmail.com>
2021-11-10 15:28:59 -0600

    AutoHotPie v1.0.4-alpha
    Moved from npm to yarn, security patches, module updates and some cleanup.

---
+++ src/PieMenu.ahk
@@ -0,0 +1,248 @@
+﻿#Persistent
+#NoEnv
+#SingleInstance force
+
+#Include %A_ScriptDir%\lib\Gdip_All.ahk
+#Include %A_ScriptDir%\lib\GdipHelper.ahk
+#Include %A_ScriptDir%\lib\BGFunks.ahk
+#Include %A_ScriptDir%\lib\PieFunctions.ahk
+#Include %A_ScriptDir%\lib\Json.ahk
+
+;Set Per monitor DPI awareness: https://www.autohotkey.com/boards/viewtopic.php?p=295182#p295182
+DllCall("SetThreadDpiAwarenessContext", "ptr", -3, "ptr")
+CoordMode, Mouse, Screen
+;  DllCall("SetThreadDpiAwarenessContext", "ptr", -3, "ptr")
+;Check AHK version and if AHK is installed.  Prompt install or update.
+
+
+
+if (!A_IsCompiled)
+	checkAHK()
+
+
+debugMode := false
+global remapLButton := ""
+
+;Read Json Settings file to object from AppData\Local\AutoHotPie
+	Try 
+	{
+		settingsFileName := "AHPSettings.json"
+		userDataFolder := A_AppData . "\AutoHotPie"
+		settingsFilePath := userDataFolder . "\" . settingsFileName
+		
+		if ( FileExist(userDataFolder) ){
+			FileRead, settings, %settingsFilePath%
+			global settings := Json.Load(settings)	
+		}else{			
+			FileCreateDir, %userDataFolder%
+			pie_openSettings()
+			exitapp
+		}
+	} catch e {
+		msgbox, % "Settings file is invalid JSON.`n`nNo Pie Menus for you :(`n`nFix settings file at:`n" . settingsFilePath
+		pie_openSettings()
+		Exitapp
+	}
+
+;Experimental
+; if FileExist(settings.global.adobeScriptsFolder)
+; 	{
+; 	copyFilesAndFolders(A_ScriptDir . "\Local Scripts\AdobePieScripts", settings.global.adobeScriptsFolder, True)
+; 	} ;Check if folder exists.
+
+;Initialize Variables and GDI+ Screen bitmap
+	;Tariq Porter, you will forever have a special place in my heart.
+	
+	global monLeft := 0
+	global monRight := 0
+	global monTop := 0
+	global monBottom := 0
+	global Mon := {left: 0,	right: 0, top: 0,bottom: 0, pieDPIScale: 1} 
+	global G ;For Gdip+ stuff
+
+
+	global pieOpenLocX	;X Position of where pie menu is opened
+	global pieOpenLocY	;Y ^
+	global subPieLocX	;X Position of where pie menu is opened
+	global subPieLocY	;Y ^
+	global subMenuActive	;Y ^
+	global activePieNumber	;Y ^
+	global ActivePieSliceHotkeyArray := [] ;loadSliceHotkeys()
+	global activeProfile
+
+	global penClicked := false
+
+	; global pieDPIScale
+	getMonitorCoords(Mon.left , Mon.right , Mon.top , Mon.bottom )
+	; getMonitorCoords(monLeft, monRight, monTop, monBottom)
+	; SetUpGDIP(monLeft, monTop, monRight-monLeft, monBottom-monTop-0.01) ;windows were appearing over taskbar without -0.01
+	SetUpGDIP(0, 0, 50, 50) ;windows were appearing over taskbar without -0.01
+	StartDrawGDIP()
+	Gdip_TextToGraphics(G, "Test", "x20 y20 Center vCenter c00FFFFFF r4 s20", "Arial")
+	ClearDrawGDIP()
+	EndDrawGDIP()
+
+	;Set up icon menu tray options
+	if (A_IsCompiled){
+		Menu, Tray, NoStandard
+		Menu, Tray, Add , AutoHotPie Settings, openSettings
+		Menu, Tray, Add , Restart, Rel
+		Menu, Tray, Add , Exit, QuitPieMenus
+		Menu, Tray, Tip , AutoHotPie		
+	}else{
+		Menu, Tray, Add , AutoHotPie Settings, openSettings
+		Menu, Tray, Add , Restart, Rel		
+	}
+	Menu, Tray, Default , AutoHotPie Settings
+	; Menu, Tray, Click, 1
+	loadPieMenus()
+return
+
+
+
+;End Initialization
+
+pieLabel: ;Fixed hotkey overlap "r and ^r", refactor this
+	; SetUpGDIP(monLeft, monTop, monRight-monLeft, monBottom-monTop, "Hide")
+	; SetUpGDIP(monLeft, monTop, monRight-monLeft, monBottom-monTop, "Show")
+	;Re-initialize variables	
+	if (pieLaunchedState == 1)
+		return
+	else
+		hotKeyArray := []	
+	pieLaunchedState := 1
+	global activePieKey := A_ThisHotkey
+	; msgbox, % activePieKey
+	
+	If (!WinActive("ahk_group regApps"))
+		{
+		profileIndex := 1
+		activeProfile := settings.appProfiles[1]
+		Hotkey, IfWinNotActive, ahk_group regApps		
+		}
+	else ;Registered applications
+		{
+		;Get application and key
+		WinGet, activeWinProc, ProcessName, A	
+		;lookup profile and key index
+		for profileIndex, profile in settings.appProfiles
+			{
+			if profileIndex == 1
+				continue			
+			; if ahk_group regApps not contains activeWindow
+			for ahkHandleIndex, ahkHandle in profile.ahkHandles
+			{
+				testAHKHandle := StrSplit(ahkHandle, " ", ,2)[2] ;Will test multiple program, may be broken
+				if (testAHKHandle == activeWinProc) ;Is this the right profile?
+					{
+						activeProfile := profile						
+						Hotkey, IfWinActive, % "ahk_exe " + activeWinProc
+						break 2	
+					}
+				}
+			}			
+		}	
+	
+	;Push hotkey to hotkeyArray to be blocked when pie menus are running.
+	for k, pieKey in activeProfile.pieKeys
+	{		
+		hotKeyArray.Push(pieKey.hotkey)
+		; msgbox, % pieKey.hotkey
+		; if (pieKey.hotkey == activePieKey)
+		; {
+		; 	for k2, pieMenu in pieKey.pieMenus
+		; 	{
+		; 		for k3, func in pieMenu.functions
+		; 		{
+		; 			if (func.hotkey != "")
+		; 			{
+		; 				; msgbox, % func.hotkey
+		; 				hotKeyArray.Push(func.hotkey)
+		; 			}					
+		; 		}				
+		; 	}
+		; }		
+	}	
+	; msgbox, % hotkeyArray[1]
+	; msgbox, % hotkeyArray[2]
+	; msgbox, % hotkeyArray[3]
+	; msgbox, % hotkeyArray[4]
+	
+	
+	for k, menus in activeProfile.pieKeys
+		{
+		if (menus.hotkey == activePieKey)
+			{			
+			blockBareKeys(activePieKey, hotKeyArray, true)			
+			funcAddress := runPieMenu(profileIndex, k)
+			pieLaunchedState := 0				
+			blockBareKeys(activePieKey, hotKeyArray, false)
+				;deactivate dummy keys
+			activePieKey := ""
+			runPieFunction(funcAddress)
+			break
+			}		
+		}
+	return	
+
+togglePieLabel:
+	pieEnableKey.modToggle()
+return
+
+onPieLabel:
+	pieEnableKey.modOn()
+	; msgbox, On
+return
+
+offPieLabel:
+	pieEnableKey.modOff()
+	; msgbox, off
+return
+
+blockLabel:
+return
+
+#If debugMode = true
+{
+escape::
+exitapp
+return
+}
+
+;Block LButton when Pie Menu is opened WHY DOESNT HOLDOPENOVERRIDE WORK???
+;I hate you so much... windows ink.
+#If (pieLaunchedState == 1)
+LButton::
+	; penClicked := true 
+	Return
+LButton up::	
+	; penClicked := false
+	Return
+;For mouseClick function
+#If (remapLButton == "Right")
+LButton::RButton
+#If (remapLButton == "Middle")
+LButton::MButton
+#If ;This ends the context-sensitivity
+
+;If a display is connected or disconnected
+OnMessage(0x7E, "WM_DISPLAYCHANGE")
+return
+WM_DISPLAYCHANGE(wParam, lParam)
+	{
+	sleep, 200
+	Reload
+	return
+	}
+
+Rel:
+	Reload
+	Return
+
+openSettings:
+	pie_openSettings()
+	Return
+
+QuitPieMenus:
+	exitapp
+	return
---
+++ src/hotkeyManagement.js
@@ -0,0 +1,353 @@
+var hotkeyManagement = {
+    resolve: null,
+    reject: null,
+    invalidAHKKeys:[],
+    hotkeyObj:{},
+    validateAHKKey:function(ahkKey){
+        let isKeyValid = (!this.invalidAHKKeys.includes(ahkKey)) ? true : false;
+        // hotkeyManagement.editKeyPage.invalidKeyWarningDiv.style.display = (isKeyValid) ? 'block' : 'none';
+        if (isKeyValid) {
+            hotkeyManagement.editKeyPage.invalidKeyWarningDiv.hide();
+            hotkeyManagement.editKeyPage.hotkeyAcceptBtn.show();
+        } else {
+            hotkeyManagement.editKeyPage.invalidKeyWarningDiv.show();
+            hotkeyManagement.editKeyPage.hotkeyAcceptBtn.hide();            
+        }
+        return isKeyValid;
+    },
+    initialize:function(){
+        this.listenForKeyPage.initialize();    
+        this.editKeyPage.initialize();    
+    },
+    listenForKeyPage:{        
+        hotkeyInputField: document.getElementById('pressed-hotkey-text-input'),
+        hotkeyClickField: document.getElementById('listen-for-click-div'),
+        cancelBtn: document.getElementById('hotkey-listen-cancel-btn'),
+        specialKeyBtn: $('#special-key-menu').children('button'),
+        specialKeyMenu: $('#special-key-menu').children('div'),
+        initialize: function(){
+            let lp = hotkeyManagement.listenForKeyPage;
+
+            initializeSpecialKeyMenu();
+            function initializeSpecialKeyMenu(){
+                specialKeys = [
+                    {
+                        key: "Back / 4th Click",
+                        keyCode: 6
+                    },
+                    {
+                        key: "Forward / 5th Click",
+                        keyCode: 7
+                    },
+                    {
+                        key: "F13",
+                        keyCode: 124
+                    },
+                    {
+                        key: "F14",
+                        keyCode: 125
+                    },
+                    {
+                        key: "F15",
+                        keyCode: 126
+                    },
+                    {
+                        key: "F16",
+                        keyCode: 127
+                    },
+                    {
+                        key: "F17",
+                        keyCode: 128
+                    },
+                    {
+                        key: "F18",
+                        keyCode: 129
+                    },
+                    {
+                        key: "F19",
+                        keyCode: 130
+                    },
+                    {
+                        key: "F20",
+                        keyCode: 131
+                    },
+                    {
+                        key: "F21",
+                        keyCode: 132
+                    },
+                    {
+                        key: "F22",
+                        keyCode: 133
+                    },
+                    {
+                        key: "F23",
+                        keyCode: 134
+                    },
+                    {
+                        key: "F24",
+                        keyCode: 135
+                    },
+                    {
+                        key: "F25",
+                        keyCode: 136
+                    },
+                    {
+                        key: "F26",
+                        keyCode: 137
+                    },
+                    {
+                        key: "F27",
+                        keyCode: 138
+                    },
+                    {
+                        key: "F28",
+                        keyCode: 139
+                    },
+                    {
+                        key: "F29",
+                        keyCode: 140
+                    },
+                    {
+                        key: "F30",
+                        keyCode: 141
+                    },
+                    {
+                        key: "F31",
+                        keyCode: 142
+                    },
+                    {
+                        key: "F32",
+                        keyCode: 143
+                    }
+                ]   
+                lp.specialKeyMenu.empty();
+                specialKeys.forEach(element => {
+                    addButtonItem(element);                                
+                });
+                function addButtonItem(specialKeyObj){
+                    lp.specialKeyMenu.append($("<a class=\"dropdown-item\" name=" + specialKeyObj.keyCode + ">" + specialKeyObj.key + "</a>"))
+                };
+                lp.specialKeyMenu.on('click', (e) => { 
+                    processHotkeyInputEvent({
+                        keyCode:parseInt(e.target.name)
+                    });
+                });
+            }
+            this.hotkeyInputField.style.opacity = 0;
+            this.hotkeyClickField.addEventListener("mouseup", function(event){
+                processHotkeyInputEvent(event);                          
+            })
+            this.hotkeyInputField.addEventListener("keyup", function(event){
+                processHotkeyInputEvent(event)
+            })
+            this.cancelBtn.addEventListener("click", function(event){                
+                hotkeyManagement.reject(false);
+            });
+        }, 
+        open:function(){            
+            $('[href="#tab-11"]').tab('show');
+            this.hotkeyInputField.focus(); 
+        },
+        
+    },
+    editKeyPage:{        
+        modBtnGroup: document.getElementById('modifier-btn-group'),
+        hotkeyDisplayField: document.getElementById('hotkey-display-field'),
+        invalidKeyWarningDiv: $('#invalid-key-warning-div'),
+        shiftBtnCheck: document.getElementById('hotkey-modifier-shift-check'),
+        ctrlBtnCheck: document.getElementById('hotkey-modifier-ctrl-check'),
+        altBtnCheck: document.getElementById('hotkey-modifier-alt-check'),
+        hotkeyCancelBtn: document.getElementById('hotkey-cancel-btn'),
+        hotkeyAcceptBtn: $('#hotkey-accept-btn'),
+        hotkeyReassignBtn: document.getElementById('hotkey-reassign-btn'),
+        initialize: function(){
+            let hm = hotkeyManagement
+            let ep = hotkeyManagement.editKeyPage;    
+            
+            this.shiftBtnCheck.addEventListener("click", function(event){                    
+                hm.hotkeyObj.isShift = ep.shiftBtnCheck.checked;
+                
+            });
+            this.ctrlBtnCheck.addEventListener("click", function(event){
+                hm.hotkeyObj.isCtrl = ep.ctrlBtnCheck.checked;                
+            });
+            this.altBtnCheck.addEventListener("click", function(event){ 
+                hm.hotkeyObj.isAlt = ep.altBtnCheck.checked;                            
+            });
+            this.hotkeyAcceptBtn.on("click", function(event){                 
+                hm.hotkeyObj.displayKey = processKeyEventToFullString()
+                function resolveAHKString(){
+                    let returnString = ""
+                    if (hm.hotkeyObj.isShift){
+                        returnString = returnString + "+"
+                    }
+                    if (hm.hotkeyObj.isCtrl){
+                        returnString = returnString + "^"                            
+                    }
+                    if (hm.hotkeyObj.isAlt){
+                        returnString = returnString + "!"
+                    }
+                    returnString = returnString + hm.hotkeyObj.ahkKey.replace(/[\!\^\+]/g,"")                        
+                    return returnString                              
+                }
+                hm.hotkeyObj.ahkKey = resolveAHKString()
+                // console.log(hotkeyObj.ahkKey)                
+                if (hotkeyManagement.validateAHKKey(hotkeyManagement.hotkeyObj.ahkKey)){
+                    hotkeyManagement.resolve(hm.hotkeyObj);
+                }                                
+            });
+            this.hotkeyReassignBtn.addEventListener("click", function(event){
+                hm.listenForKeyPage.open()
+            });
+            this.hotkeyCancelBtn.addEventListener("click", function(event){
+                hotkeyManagement.reject();
+            });
+        },
+        open:function(){            
+            $('[href="#tab-7"]').tab('show'); 
+            this.refresh();       
+        },
+        refresh: function(){
+            let hm = hotkeyManagement;
+            this.shiftBtnCheck.checked = hm.hotkeyObj.isShift;           
+            this.ctrlBtnCheck.checked = hm.hotkeyObj.isCtrl;            
+            this.altBtnCheck.checked = hm.hotkeyObj.isAlt; 
+            this.hotkeyDisplayField.value = hm.hotkeyObj.displayKeyNoMods;
+        }
+    }
+}
+
+// var rejectHotkeyPromise, resolveHotkeyPromise;
+
+function processHotkeyInputEvent(event){
+    hm = hotkeyManagement;    
+    var keyNumber = event.keyCode    
+    if (event.type == "mouseup"){//Mousebutton        
+        
+        if (event.button == 0){
+            hm.listenForKeyPage.hotkeyInputField.focus();
+            return} //Don't allow LMB click
+        keyNumber = event.button + 3
+        event.code = "mousebutton"
+        event.keyCode = event.button + 3
+    }else{ //Keystroke
+        var ignoreKeys = ['Control','Shift','Alt']
+        if (ignoreKeys.includes(event.key)){
+            hm.listenForKeyPage.hotkeyInputField.focus();
+            return}
+        keyNumber = event.keyCode                    
+    } 
+    
+    hotkeyManagement.hotkeyObj = processHotkey(event)
+    
+    
+    hm.editKeyPage.hotkeyAcceptBtn.focus()
+    
+    //Intitalize value of input fields
+
+    hotkeyManagement.editKeyPage.open();
+    hotkeyManagement.validateAHKKey(hotkeyManagement.hotkeyObj.ahkKey);
+    hotkeyInputField = document.getElementById('pressed-hotkey-text-input');
+};
+function processKeyEventToFullString(){ // Refactor pls
+        hotkeyObj = hotkeyManagement.hotkeyObj
+        let returnString = ""
+        if (hotkeyObj.isShift){
+            returnString = returnString + "Shift+"
+        }
+        if (hotkeyObj.isCtrl){
+            returnString = returnString + "Ctrl+"
+        }
+        if (hotkeyObj.isAlt){
+            returnString = returnString + "Alt+"
+        }                        
+        returnString = returnString + hotkeyObj.displayKeyNoMods
+        return returnString
+    }
+function processHotkey(keyEvent){
+        let keyTableArray = AutoHotPieSettings.global.htmlAhkKeyConversionTable;
+        let keyNumber = (keyEvent.code == "Enter") ? 1 : keyEvent.keyCode        
+        
+        // console.log(keyNumber)
+        let allowModifiers = (hotkeyManagement.editKeyPage.modBtnGroup.style.display == "block") ? true : false;
+        let keyObj = keyTableArray.find(x => x.keyCode === keyNumber)                    
+
+        function processKeyEventToAHKString(){
+            let returnString = ""
+            if (keyEvent.shiftKey){
+                returnString = returnString + "+"
+            }
+            if (keyEvent.ctrlKey){
+                returnString = returnString + "^"
+            }
+            if (keyEvent.altKey){
+                returnString = returnString + "!"
+            }
+            returnString = returnString + keyObj.ahkKey
+            return returnString                              
+        }
+        
+        function processKeyEventToFullString(){
+            let returnString = ""
+            if (keyEvent.shiftKey){
+                returnString = returnString + "Shift+"
+            }
+            if (keyEvent.ctrlKey){
+                returnString = returnString + "Ctrl+"
+            }
+            if (keyEvent.altKey){
+                returnString = returnString + "Alt+"
+            }                        
+            returnString = returnString + keyObj.displayKey
+            return returnString
+        } 
+        if(allowModifiers){
+            return {
+                isShift:keyEvent.shiftKey,                        
+                isCtrl:keyEvent.ctrlKey,                        
+                isAlt:keyEvent.altKey,
+                keyCode:keyObj.keyCode,
+                displayKey:processKeyEventToFullString(),
+                displayKeyNoMods:keyObj.displayKey,
+                ahkKey:processKeyEventToAHKString()
+            }
+        }else{
+            return {
+                isShift:false,                        
+                isCtrl:false,                        
+                isAlt:false,
+                keyCode:keyObj.keyCode,
+                displayKey:keyObj.displayKey,
+                displayKeyNoMods:keyObj.displayKey,
+                ahkKey:keyObj.ahkKey
+            }
+        }
+    }
+
+async function assignKey(options){
+    var defaults = {
+        allowModifiers:true,
+        invalidAHKKeys:[],
+        warningText:"This key is already in use, edit or choose another key."
+    }    
+    var setting = Object.assign({}, defaults, options);
+    let hm = hotkeyManagement;
+    hotkeyManagement.invalidAHKKeys = setting.invalidAHKKeys;    
+    // hm.editKeyPage.invalidKeyWarningDiv.html(setting.warningText);    
+    // hm.editKeyPage.invalidKeyWarningDiv.hidden = false;    
+    hotkeyManagement.editKeyPage.modBtnGroup.style.display = (setting.allowModifiers) ? 'block' : 'none';
+    
+    hotkeyManagement.listenForKeyPage.open();
+    let myPromise = await waitForUserInput()
+    return myPromise
+    function waitForUserInput(){
+        return new Promise((resolve, reject) => {
+            hotkeyManagement.resolve = resolve;
+            hotkeyManagement.reject = reject;
+        });
+    }       
+}
+
+hotkeyManagement.initialize();
+
+
