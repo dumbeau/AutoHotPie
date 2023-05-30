b2e6614683d33224ed1459a21e1ff84fd10bac49
ryjacky <ryjacky@outlook.com>
2023-05-31 00:16:18 +0900

    UI updates

--- src/app/pie-menu-editor/pie-menu-editor.module.ts
+++ src/app/pie-menu-editor/pie-menu-editor.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PieMenuEditorComponent } from './pie-menu-editor.component';
 import {TreeModule} from '@circlon/angular-tree-component';
+import {TranslateModule} from "@ngx-translate/core";
+import {PreferencesEditorComponent} from "./preferences-editor/preferences-editor.component";
+import {NbListModule, NbToggleModule} from "@nebular/theme";
 
 @NgModule({
   declarations: [
-    PieMenuEditorComponent
+    PieMenuEditorComponent,
+    PreferencesEditorComponent
   ],
   imports: [
     CommonModule,
-    TreeModule
+    TreeModule,
+    TranslateModule,
+    NbListModule,
+    NbToggleModule
   ]
 })
 export class PieMenuEditorModule { }
