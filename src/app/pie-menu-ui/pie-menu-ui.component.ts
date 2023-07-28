3153616c358c73ed35e6bdbe38a5dcb94f2832a2
Beau Gilles <36665762+dumbeau@users.noreply.github.com>
2023-07-28 05:20:05 -0500

    pie-menu-ui updates
    Added a few simple properties to pie-menu-ui component.  Ttesting a merge.

--- src/app/pie-menu-ui/pie-menu-ui.component.ts
+++ src/app/pie-menu-ui/pie-menu-ui.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Konva } from "konva/lib/Global";
 
 @Component({
   selector: 'app-pie-menu-ui',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./pie-menu-ui.component.scss']
 })
 export class PieMenuUIComponent {
+  isVisible = false;
+  xPosition = 0;
+  yPosition = 0;
+  hotkey = 'Control+M';
 
+  isInEditor = false;
+  editorXPosition = 0;
+  editorYPosition = 0;
+  overlayXPosition = 0;
+  overlayYPosition = 0;
+
+  showOverlay(x: number, y: number) {
+    this.isVisible = true;
+    this.xPosition = x;
+    this.yPosition = y;
+  }
+
+  hideOverlay() {
+    this.isVisible = false;
+  }
+
+  onHotkeyPress(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'm') {
+      this.isVisible = !this.isVisible;
+    }
+  }
+
+  drawInEditor(x: number, y: number) {
+    this.isInEditor = true;
+    this.editorXPosition = x;
+    this.editorYPosition = y;
+  }
+
+  drawInOverlay(x: number, y: number) {
+    this.isInEditor = false;
+    this.overlayXPosition = x;
+    this.overlayYPosition = y;
+  }
 }
