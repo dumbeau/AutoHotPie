431f753ac623ae85621ab4423ebbfdfc55fd6ce8
Beau Gilles <36665762+dumbeau@users.noreply.github.com>
2024-01-15 21:39:13 -0600

    Moved build-ahk.bat
    Made the build script for the AHK pie menus and updated scripts accordingly.

---
+++ build/build-ahk.bat
@@ -0,0 +1,1 @@
+"%ProgramFiles%\AutoHotkey\Compiler\Ahk2Exe.exe" /in "src\PieMenu.ahk" /out "src\PieMenu.exe" /icon "src\assets\AutoHotPieIcon.ico" /bin "%ProgramFiles%\AutoHotkey\Compiler\Unicode 64-bit.bin"
